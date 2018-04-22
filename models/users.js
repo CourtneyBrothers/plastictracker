646e7e02f989045f3065a716009caef66eec4267
Courtney Brothers <courtneywbrothers@gmail.com>
2018-04-22 05:48:38 -0500

    reconfig database

--- models/users.js
+++ models/users.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
     username: DataTypes.TEXT,
+    email: DataTypes.STRING,
     password: DataTypes.STRING
   }, {tableName: "users", timestamps: false});
   User.associate= (models) => {
