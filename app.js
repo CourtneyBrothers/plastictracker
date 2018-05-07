eac76668ec435a08295eb1383721e73fa8b65fe3
Courtney Brothers <courtneywbrothers@gmail.com>
2018-05-07 15:11:41 -0500

    add watch task to sass

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(session({
 })); // session secret
 
 
-app.use('/public', express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(
   "/bootstrap",
