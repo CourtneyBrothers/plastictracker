bf82f09de65781258cbf4ae85cfc388fb3db21f1
Courtney Brothers <courtneywbrothers@gmail.com>
2018-05-11 13:40:31 -0500

    change icons on click on sup detail 4

---
+++ client/clientapp/sup.js
@@ -0,0 +1,13 @@
+console.log('hi');
+console.log(document.getElementsByClassName("reuseBtn"), "class");
+let reuseBtns = document.getElementsByClassName("reuseBtn");
+
+Array.from(reuseBtns).forEach((btn) => {
+  console.log(btn, "btn")
+  btn.addEventListener('click', (e) => {
+      console.log(e, "event")
+      console.log((e.target.id), "id");
+      document.getElementById(`sup${e.target.id}`).src="/images/wave.svg";
+    }
+  )
+});
