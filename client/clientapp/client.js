b9e95840df1c51bbba9d15e95dec2ffae8fd9e5b
Courtney Brothers <courtneywbrothers@gmail.com>
2018-05-17 04:30:48 -0500

    change reuse buttons on all detail pages

--- client/clientapp/client.js
+++ client/clientapp/client.js
@@ -7,19 +7,3 @@ document.getElementById("showLogin").addEventListener('click', () => {
 document.getElementById("showRegister").addEventListener('click', () => {
   location.href = `${location.origin}/register`;
 });
-
-
-document.getElementById("swapReuse").addEventListener('click',()=>{
-  document.getElementById("drink").src="/images/002-water.svg";
-  document.getElementById("reused").innerHTML="total reused: 1";
-})
-
-document.getElementById("swapRecycle").addEventListener('click',()=>{
-  document.getElementById("drink").src="/images/recycle-bin.svg";
-  document.getElementById("recycled").innerHTML="total recycled: 1";
-})
-
-document.getElementById("my700").addEventListener('click',()=>{
-  document.getElementById("drink1").src="/images/wave.svg";
-  document.getElementById("text700").innerHTML="My 700:<br> 466";
-})
