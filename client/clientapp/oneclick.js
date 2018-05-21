82802af609b304d0bba784113144282ec3a18cce
Courtney Brothers <courtneywbrothers@gmail.com>
2018-05-21 14:10:44 -0500

    update

--- client/clientapp/oneclick.js
+++ client/clientapp/oneclick.js
@@ -2,25 +2,25 @@ let oneClickBtns = document.getElementsByClassName("oneClickBtn");
 
 Array.from(oneClickBtns).forEach((btn) => {
   btn.addEventListener('click', (e) => {
-      document.getElementById(`img${e.target.id}`).src="/images/one.svg";
-    }
-  )
+    document.getElementById(`img${e.target.id}`).src = "/images/one.svg";
+  })
 });
 
 let oneClickCards = document.getElementsByClassName("oneClick");
 
 Array.from(oneClickCards).forEach((card) => {
   card.addEventListener('click', (e) => {
-      let imgNumber = +e.target.id.split("img")[1];
-      console.log('e',+e.target.id.split("img")[1]);
-      document.getElementById(`img${imgNumber}`).src="/images/one.svg";
-      $.ajax({
-        url:`${location.origin}/savedClick/${imgNumber}`,
-        type:'post',
-        data:{}
-      }).done(()=>{
-        window.location.href=`${location.origin}/saved/${imgNumber}`
-      })
-    }
-  )
+    let imgNumber = +e.target.id.split("img")[1];
+    console.log('e', +e.target.id.split("img")[1]);
+    document.getElementById(`img${imgNumber}`).src = "/images/one.svg";
+    $.ajax({
+      url: `${location.origin}/savedClick/${imgNumber}`,
+      type: 'post',
+      data: {}
+    }).done(() => {
+      setTimeout(() => {
+        window.location.href = `${location.origin}/saved/${imgNumber}`
+      }, 1)
+    })
+  })
 });
